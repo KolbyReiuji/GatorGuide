8dae60cb3106e5d60ab6a31d1a5a894ccd969bba
Khoi Le <107971174+KolbyReiuji@users.noreply.github.com>
2026-03-12 14:44:45 -0700

    change a lil bit GatorGuideV2/DataScraping/Server/GatorGuide/API/models.py

--- GatorGuideV2/DataScraping/Server/GatorGuide/API/models.py
+++ GatorGuideV2/DataScraping/Server/GatorGuide/API/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# Yeah bro pls change all the field inside there, this is just an example to test out xd
 class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)

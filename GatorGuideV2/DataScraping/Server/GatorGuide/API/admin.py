16143243fd3b962b5bf4ceea1bb01895a2d1cb73
Khoi Le <107971174+KolbyReiuji@users.noreply.github.com>
2026-03-14 11:59:13 -0700

    Modified admin.py

--- GatorGuideV2/DataScraping/Server/GatorGuide/API/admin.py
+++ GatorGuideV2/DataScraping/Server/GatorGuide/API/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-# from .models import User
+from .models import User, CostOfAttendance, School
 
-# # Register your models here.
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('username', 'email', 'password')
+# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    # It is safer to exclude the password from the list view
+    list_display = ('username', 'email') 
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name','address')

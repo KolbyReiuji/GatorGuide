4215886f372b0d2249b8387b3728a1c9952894cd
Khoi Le <107971174+KolbyReiuji@users.noreply.github.com>
2026-03-14 10:58:25 -0700

    View_sender update

--- GatorGuideV2/DataScraping/Server/GatorGuide/API/views_sender.py
+++ GatorGuideV2/DataScraping/Server/GatorGuide/API/views_sender.py
@@ -1,11 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from .serializer import UserSerializer
-from .models import User
-# Create your views here.
+# from .serializer import UserSerializer
+# from .models import User
+# # Create your views here.
 
-# Write your bunch of GET function here
-@api_view(['GET'])
-def get_users(request):
-    return Response(UserSerializer(User.objects.all(), many=True).data)
+# # Write your bunch of GET function here
+# @api_view(['GET'])
+# def get_users(request):
+#     return Response(UserSerializer(User.objects.all(), many=True).data)

23552c027739448d17a82f651fbf39a7b108f352
anhvo2211 <vongocminhanh2211@gmail.com>
2026-03-18 18:08:41 -0700

    Until Now

--- GatorGuideV2/DataScraping/Server/API/urls_handler.py
+++ GatorGuideV2/DataScraping/Server/API/urls_handler.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views_handler
 
 urlpatterns = [
-    # example path
-    # path('user/create/', views_handler.create_user, name='create_user'),
+    path('user/create', views_handler.create_user, name = "create"),
+    path('user/delete', views_handler.delete_user, name = "delete"),
+    path('user/update', views_handler.update_user, name = "delete")
 ]

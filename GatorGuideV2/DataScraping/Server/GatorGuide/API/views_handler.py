3cf3156d1a31780354cf71533081a9adb2ba6a90
anhvo2211 <vongocminhanh2211@gmail.com>
2026-03-18 17:26:32 -0700

    Update views_handler.py

--- GatorGuideV2/DataScraping/Server/GatorGuide/API/views_handler.py
+++ GatorGuideV2/DataScraping/Server/GatorGuide/API/views_handler.py
@@ -16,7 +16,28 @@ def create_user(request):
 
 # @api_view(['PUT'])
 # Write your update_user function here
+@api_view(['PUT'])
+def update_user(request, pk):
+    try:
+        existed_user = User.objects.get(pk = pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
+    serializer = UserSerializer(existed_user, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # @api_view(['DELETE'])
 # Write your delete_user function here
+@api_view(['DELETE'])
+def delete_user(request, pk):
+    try:
+        existed_user = User.objects.get(pk = pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = UserSerializer(existed_user)
+    existed_user.delete()
+    return Response(serializer.data, status=status.HTTP_200_OK)
--- GatorGuideV2/DataScraping/Server/GatorGuide/API/views_sender.py
+++ GatorGuideV2/DataScraping/Server/GatorGuide/API/views_sender.py
@@ -9,3 +9,28 @@ from .models import User
 @api_view(['GET'])
 def get_users(request):
     return Response(UserSerializer(User.objects.all(), many=True).data)
+
+# @api_view(['GET'])
+# def get_user(request, pk):
+#     try:
+#         user = User.objects.get(pk = pk)
+#     except User.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     serializer = UserSerializer(user)
+#     return Response(serializer.data, status=status.HTTP_200_OK)
+
+# @api_view(['GET'])
+# def get_school(request, pk):
+#     try:
+#         school = School.objects.get(pk = pk)
+#     except School.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     serializer = SchoolSerializer(school)
+#     return Response(serializer.data, status=status.HTTP_200_OK)
+
+# @api_view(['GET'])
+# def get_all_school(request):
+#     schools = School.objects.values('name','school_id')
+#     return Response(list(schools), status=status.HTTP_200_OK)
